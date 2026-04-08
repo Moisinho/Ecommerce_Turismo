4e7ee721a8469dbd749c70b9eb1a929a44230b7a
MoiB03 <102995807+Moisinho@users.noreply.github.com>
2026-04-07 21:36:50 -0500

    Update nosotros.jsx

--- ecommerce/src/pages/nosotros.jsx
+++ ecommerce/src/pages/nosotros.jsx
@@ -5,7 +5,7 @@ function Nosotros() {
     <div className="w-full min-h-screen bg-gradient-to-r from-blue-500 to-purple-500 px-4">
       <h2 className="text-4xl font-semibold text-center text-white pt-8 mb-10">Conoce a Nuestro Equipo</h2>
 
-      <div className="flex justify-between space-x-6">
+      <div className="flex justify-center gap-8">
         {/* Miembro 1 */}
         <div className="bg-white rounded-lg shadow-lg overflow-hidden p-6 w-1/3 flex flex-col items-center mb-6">
           <img 
@@ -104,56 +104,6 @@ function Nosotros() {
                 </ul>
                 </div>
           </div>
-        </div>
-
-        {/* Miembro 3 */}
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden p-6 w-1/3 flex flex-col items-center mb-6">
-          <img 
-            className="w-52 h-48 object-cover mb-8 rounded-full border-4 border-green-400" 
-            src={require('../assets/img/nadim.png')} 
-            alt="Nadim García"
-          />
-          <div className="p-4 h-32 text-center w-full">
-            <h3 className="text-2xl font-bold mb-2 text-green-600">Nadim García</h3>
-            <p className="text-gray-600">
-              Estudiante de Licenciatura en Desarrollo de Software en la Universidad Tecnológica de Panamá.
-            </p>
-          </div>
-
-          <div className="overflow-y-auto max-h-96 w-full px-4">
-              <div className="p-4">
-                <h4 className="text-lg font-bold mb-2 text-green-600">Contacto</h4>
-                <ul className="text-gray-600">
-                  <li>Teléfono: +507 6508-8531</li>
-                  <li>Correo: nadim.garcia@utp.ac.pa</li>
-                </ul>
-              </div>
-
-              <div className="p-4">
-                <h4 className="text-lg font-bold mb-2 text-green-600">Conocimientos</h4>
-                <ul className="list-disc list-inside text-gray-600">
-                  <li>Nivel avanzado en JavaScript y frameworks modernos</li>
-                  <li>Experiencia en diseño responsivo con Tailwind CSS</li>
-                  <li>Desarrollo web Full Stack con React y Node.js</li>
-                  <li>Uso avanzado de herramientas de control de versiones como Git</li>
-                  <li>Inglés técnico intermedio</li>
-                  <li>Desarrollo de aplicaciones móviles con Flutter</li>
-                </ul>
-              </div>
-
-              <div className="p-4">
-                <h4 className="text-lg font-bold mb-2 text-green-600">Habilidades</h4>
-                <ul className="list-disc list-inside text-gray-600">
-                  <li>Organización y gestión de tareas</li>
-                  <li>Trabajo en equipo multidisciplinario</li>
-                  <li>Comunicación efectiva</li>
-                  <li>Innovación y creatividad</li>
-                  <li>Capacidad de aprendizaje autónomo</li>
-                  <li>Resolución de conflictos</li>
-                  <li>Trabajo bajo presión</li>
-                </ul>
-              </div>
-            </div>
         </div>
       </div>
     </div>
